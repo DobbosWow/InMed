dee112574978dc82336d56570f4b1e8b16797c0e
DobbosWow <dobbos.wow@gmail.com>
2018-03-18 13:38:06 +0500

    Created mainform

--- InMed_Forms/InMed_Forms/Program.cs
+++ InMed_Forms/InMed_Forms/Program.cs
@@ -16,6 +16,7 @@ namespace InMed_Forms
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+            Application.Run(new Form2());
         }
     }
 }
