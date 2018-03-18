899bfd2d061b61351faa9554b7ef440669a0e16b
DobbosWow <dobbos.wow@gmail.com>
2018-03-18 15:32:36 +0500

    created new class Patient add Patient value on Form1

--- InMed_Forms/InMed_Forms/Form1.cs
+++ InMed_Forms/InMed_Forms/Form1.cs
@@ -11,16 +11,25 @@ namespace InMed_Forms
 {
     public partial class Form1 : Form
     {
-        public Form1()
+        private Pacient initIt = new Pacient();
+        public Form1(Pacient myPac)
         {
             InitializeComponent();
+            initIt.Name = myPac.Name;
+            initIt.Secname = myPac.Secname;
+            initIt.Surname = myPac.Surname;
+        }
+
+        private void changeval()
+        {
+            label1.Text = initIt.Name;
+            label2.Text = initIt.Secname;
+            label3.Text = initIt.Surname;
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            Form f = new Form();
-            f.Text = "Mame privet";
-            f.Show();
+            changeval();
         }
     }
 }
--- InMed_Forms/InMed_Forms/Program.cs
+++ InMed_Forms/InMed_Forms/Program.cs
@@ -5,6 +5,42 @@ using System.Windows.Forms;
 
 namespace InMed_Forms
 {
+    public class Pacient
+    {
+        public enum pacgender { male, female };
+        private string name, surname, secondname;
+        private pacgender pacGender;
+        private int pacAge;
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public string Surname
+        {
+            get { return surname; }
+            set { surname = value; }
+        }
+
+        public string Secname
+        {
+            get { return secondname; }
+            set { secondname = value; }
+        }
+        public pacgender pacg
+        {
+            get { return pacGender; }
+            set { pacGender = value; }
+        }
+        public int Age
+        {
+            get { return pacAge; }
+            set { pacAge = value; }
+        }
+
+
+    }
     static class Program
     {
         /// <summary>
@@ -13,10 +49,15 @@ namespace InMed_Forms
         [STAThread]
         static void Main()
         {
+            Pacient myp = new Pacient();
+            myp.Name = "Ivan";
+            myp.pacg = 0;
+            myp.Secname = "Ivanivich";
+            myp.Surname = "Ivanov";
+            myp.Age = 45;
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
-            Application.Run(new Form2());
+            Application.Run(new Form1(myp));
         }
     }
 }
